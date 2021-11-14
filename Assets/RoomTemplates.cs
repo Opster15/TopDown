5443617f75b8c1f9a7715d3207c8a95e235f89e6
Owen <opster15@gmail.com>
2021-11-14 18:30:25 +0000

    Rl Stuff

---
+++ Assets/RoomTemplates.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomTemplates : MonoBehaviour
+{
+
+    public GameObject[] bRooms;
+    public GameObject[] tRooms;
+    public GameObject[] lRooms;
+    public GameObject[] rRooms;
+
+    public GameObject closedRoom;
+
+    public List<GameObject> rooms;
+}
--- Assets/Scripts/Stage/RoomSpawner.cs
+++ Assets/Scripts/Stage/RoomSpawner.cs
@@ -4,17 +4,67 @@ using UnityEngine;
 
 public class RoomSpawner : MonoBehaviour
 {
-    public GameObject[] rooms;
-    public GameObject room;
-    public GameObject roomClone;
+    private RoomTemplates templates;
+    
+    public int openingDirection;
+    //1 = bottom
+    //2 = top
+    //3 = left
+    //4 = right
+    private int random;
+    private bool spawned = false;
 
-
-    int random;
-
-    private void Awake()
+    void Start()
     {
-        random = Random.Range(0, rooms.Length);
-        room = rooms[random];
-        roomClone = (GameObject)Instantiate(room, transform.position, transform.rotation);
+        templates = FindObjectOfType<RoomTemplates>();
+        Invoke("Spawn", 2f);
+    }
+
+    void Spawn()
+    {
+        if(spawned == false)
+        {
+            if (openingDirection == 1)
+            {
+                random = Random.Range(0, templates.bRooms.Length);
+                Instantiate(templates.bRooms[random], transform.position, templates.bRooms[random].transform.rotation);
+            }
+            else if (openingDirection == 2)
+            {
+                random = Random.Range(0, templates.bRooms.Length);
+                Instantiate(templates.tRooms[random], transform.position, templates.tRooms[random].transform.rotation);
+            }
+            else if (openingDirection == 3)
+            {
+                random = Random.Range(0, templates.bRooms.Length);
+                Instantiate(templates.lRooms[random], transform.position, templates.lRooms[random].transform.rotation);
+            }
+            else if (openingDirection == 4)
+            {
+                random = Random.Range(0, templates.bRooms.Length);
+                Instantiate(templates.rRooms[random], transform.position, templates.rRooms[random].transform.rotation);
+            }
+            spawned = true;
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("SpawnPoint"))
+        {
+            if(other.GetComponent<RoomSpawner>().spawned == false && spawned == false)
+            {
+                Invoke("ClosedRoom", 1f);
+                Destroy(gameObject);
+            }
+            spawned = true;
+        }
+    }
+
+    void ClosedRoom()
+    {
+        Instantiate(templates.closedRoom, transform.position, Quaternion.identity);
+
     }
 }
